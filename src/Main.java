263ca0ac13aa0be4fc9da56ec258111f306f8edb
CulesBao <102230334@sv1.dut.udn.vn>
2025-05-02 14:36:23 +0700

    fix: chore fix

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import CONFIG.SeedData;
 public class Main {
   public static void main(String[] args) {
     GlobalExceptionHandler.setup();
-    new GUI.AdminDashboard();
+    new GUI.LoginForm();
     new SeedData();
   }
 }
